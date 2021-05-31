5508be51f8e824bc06766437dd237e2995f82b3d
nyan-rk <alss-dipsw20-rny@ccicampus.fr>
2021-05-31 22:21:16 +0200

    1.4 Integrate modal after clicking on place (circle)/Added button and styling

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\MapRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class MapController extends AbstractController
 {
-    public function index(): Response
+    public function index
+    (
+        MapRepository $mapRepository
+    ): Response
     {
+
+        $isValided = $mapRepository->findAll();
+
         return $this->render('map/map.html.twig', [
-            'map' => 'MapController',
+            'map' => $isValided,
         ]);
     }
 }
