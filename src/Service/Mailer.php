9e6942a689734eea3a13e85ce54c61ee60cdd416
nomislav <nomislav.pro@gmail.com>
2021-07-09 11:07:32 +0200

    Fixing Mail Confirmation

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -21,7 +21,7 @@ class Mailer {
     {
 
         $email = (new TemplatedEmail())
-            ->from('cecile.schwinte@gmail.com')
+            ->from(new Address('crm.lite.cci@gmail.com', 'LeChenantier'))
             ->to(new Address($email))
             ->subject('Veuillez confirmer votre inscription !')
 
